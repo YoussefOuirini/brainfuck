e423852270d18c1ef9a82d0a5eca0516e36fcd9a
Youssef Ouirini <youssefouirini@gmail.com>
2021-06-28 23:38:44 +0200

    test case for when error

--- cmd/tests/compile_test.go
+++ cmd/tests/compile_test.go
@@ -9,8 +9,13 @@ import (
 
 func Test_CompileBf(t *testing.T) {
 	t.Run("runs with no error", func(t *testing.T) {
-		program, err := cmd.CompileBf("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
-		t.Log(program)
+		_, err := cmd.CompileBf("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
 		assert.NoError(t, err)
+	})
+
+	t.Run("errors when no jumpstack", func(t *testing.T) {
+		_, err := cmd.CompileBf("[")
+		assert.Error(t, err)
+		assert.EqualError(t, err, "compilation error: jumpStack is not 0")
 	})
 }
