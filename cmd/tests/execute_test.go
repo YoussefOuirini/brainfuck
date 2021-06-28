cb2e5757e72d1db23c721df70e3864899dc311f3
Youssef Ouirini <youssefouirini@gmail.com>
2021-06-29 00:37:26 +0200

    updating tests

--- cmd/tests/execute_test.go
+++ cmd/tests/execute_test.go
@@ -5,18 +5,19 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/youssefouirini/brainfuck/cmd"
+	"github.com/youssefouirini/brainfuck/model"
 )
 
 func Test_ExecuteBf(t *testing.T) {
 	t.Run("succes runs Hello World!", func(t *testing.T) {
-		testContent := []byte("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+		testContent := model.Brainfuck{Contents: []byte("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")}
 		result, err := cmd.ExecuteBf(testContent)
 		assert.NoError(t, err)
 		assert.Equal(t, "Hello World!\n", result)
 	})
 
 	t.Run("666", func(t *testing.T) {
-		testContent := []byte(">+++++++++[<++++++>-]<...>++++++++++.")
+		testContent := model.Brainfuck{Contents: []byte(">+++++++++[<++++++>-]<...>++++++++++.")}
 		result, err := cmd.ExecuteBf(testContent)
 		assert.NoError(t, err)
 		assert.Equal(t, "666\n", result)
