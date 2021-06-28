3d59aef3a3acdbd5cffdc276db12f193a4573d1e
Youssef Ouirini <youssefouirini@gmail.com>
2021-06-28 16:35:02 +0200

    updating constant names

--- cmd/execute.go
+++ cmd/execute.go
@@ -6,33 +6,33 @@ import (
 	"os"
 )
 
-const data_size int = 65535
+const dataSize int = 65535
 
 func ExecuteBf(program []Instruction) {
-	data := make([]int16, data_size)
-	var data_ptr uint16 = 0
+	data := make([]int16, dataSize)
+	var dataPointer uint16 = 0
 	reader := bufio.NewReader(os.Stdin)
 	for pc := 0; pc < len(program); pc++ {
 		switch program[pc].operator {
-		case op_inc_dp:
-			data_ptr++
-		case op_dec_dp:
-			data_ptr--
-		case op_inc_val:
-			data[data_ptr]++
-		case op_dec_val:
-			data[data_ptr]--
-		case op_out:
-			fmt.Printf("%c", data[data_ptr])
-		case op_in:
+		case increasePointer:
+			dataPointer++
+		case decreasePointer:
+			dataPointer--
+		case increaseValue:
+			data[dataPointer]++
+		case decreaseValue:
+			data[dataPointer]--
+		case out:
+			fmt.Printf("%c", data[dataPointer])
+		case in:
 			read_val, _ := reader.ReadByte()
-			data[data_ptr] = int16(read_val)
-		case op_jmp_fwd:
-			if data[data_ptr] == 0 {
+			data[dataPointer] = int16(read_val)
+		case jumpForward:
+			if data[dataPointer] == 0 {
 				pc = int(program[pc].operand)
 			}
-		case op_jmp_bck:
-			if data[data_ptr] > 0 {
+		case jumpBackward:
+			if data[dataPointer] > 0 {
 				pc = int(program[pc].operand)
 			}
 		default:
